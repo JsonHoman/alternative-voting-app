b420f0149cf38620ee7493f733b2cb68f9f6c216
Jason Homan <homan.jason12@gmail.com>
2023-09-08 10:41:37 -0400

    spring boot init

---
+++ src/main/java/com/ravtech/stvapp/StvAppApplication.java
@@ -0,0 +1,13 @@
+package com.ravtech.stvapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StvAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StvAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravtech/stvapp/StvAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravtech.stvapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StvAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
